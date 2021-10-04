b210753d820c23d852f27349b910024954704eed
eomsunyoung <kuroich123@naver.com>
2021-10-04 13:17:02 +0900

    flaskweb

--- example/start_app.py
+++ example/start_app.py
@@ -7,4 +7,12 @@ def loopindex():
     items = ['a', 'b', 'c', 'd']
     return render_template('loopindex.html', items=items)
 
+@app.route('/node')
+def node():
+    return render_template('node.html')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 app.run()
